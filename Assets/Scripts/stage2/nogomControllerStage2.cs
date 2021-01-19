3f3503b37568b7b7a3355044bb9e04e79ba0a83b
Hansle Lee <hssarah13@gmail.com>
2021-01-20 01:50:30 +0900

    update

---
+++ Assets/Scripts/stage2/nogomControllerStage2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nogomControllerStage2 : MonoBehaviour
+{
+    Vector2 nogomPos;  // nogom's position
+    float moveSpeed = 0.03f;
+
+    void Start()
+    {
+        nogomPos = transform.position;
+    }
+
+    void Update()
+    {
+        if(Input.touchCount>0&&Input.GetTouch(0).phase==TouchPhase.Moved)
+        {
+            float dist = transform.position.y - Camera.main.transform.position.y;
+        }
+    }
+}
